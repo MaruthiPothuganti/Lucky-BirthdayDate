51d4c73f8c64bb68af84dbd1dfa48570d9562cbc
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2021-08-13 07:19:14 +0530

    feat: Next Button Disable/Enable
    "Next" button  only enables after taking a Date input from user

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ const userBirthDate = document.querySelector("#birthDate");
 const userLuckyNumber = document.querySelector("#luckyNumber");
 const btnCheckLuck = document.querySelector("#check");
 const message = document.querySelector("#outputMessage");
+const btnNext = document.querySelector("#next");
+
+const birthDate = userBirthDate.toString();
+btnNext.disabled = "true";
+
+function enable() {
+    if (birthDate.trim() === "") {
+        btnNext.disabled = "true";
+    } else {
+        btnNext.disabled = "";
+    }
+}
+btnNext.addEventListener("click", function activateNextInput() {
+    btnCheckLuck.style.display = "inline";
+    userLuckyNumber.style.display = "inline";
+
+});
 
 
 
 btnCheckLuck.addEventListener("click", function checkIfLucky() {
     const dob = userBirthDate.value;
 
+
     const totalSum = sumDate(dob);
     if (totalSum % (Number(userLuckyNumber.value)) === 0) {
-        showMessage("YOUR BIRTHDAY IS LUCKY😉");
+        showMessage("YOUR BIRTHDAY IS LUCKY🥳🤩");
     } else {
         showMessage("Uh,oh.. Sorry, BetterLuck in NextLife😅");
     }
